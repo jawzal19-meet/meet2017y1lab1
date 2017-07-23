036ac84ad771dd4441a7de09a09f203fda3cf286
jawzal19-meet <jawzal19@meet.mit.edu>
2017-07-23 22:25:35 +0300

    Stopping here

---
+++ MEETinTurtle.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.penup
+turtle.goto(0,0)
+turtle.pendown
+turtle.goto(-200,-100+200)
+turtle.goto(-200+50,-100)
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+turtle.penup
+turtle.goto(-250,-100+200)
+turtle.pendown
+turtle.goto(-300,-100+200)
+turtle.mainloop()
+
